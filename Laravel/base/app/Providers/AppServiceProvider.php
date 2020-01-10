142b3837fabcd8a36fc5f193318b8b6cc87439e9
Matthieu Napoli <matthieu@mnapoli.fr>
2020-01-10 17:50:53 +0400

    Add Laravel example

---
+++ Laravel/base/app/Providers/AppServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+
+        // Make sure the directory for compiled views exists
+        if (! is_dir(config('view.compiled'))) {
+            mkdir(config('view.compiled'), 0755, true);
+        }
+    }
+}
