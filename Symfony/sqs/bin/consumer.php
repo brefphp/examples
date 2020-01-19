1db16360b283c1f184315dbcc4889b3cc1e3d53d
Nyholm <tobias.nyholm@gmail.com>
2020-01-19 13:45:26 +0100

    Use bref/symfony-messenger

--- Symfony/sqs/bin/consumer.php
+++ Symfony/sqs/bin/consumer.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-use Bref\Messenger\Sqs\SqsConsumer;
+use Bref\Messenger\Service\BrefWorker;
 
 require dirname(__DIR__) . '/config/bootstrap.php';
 
@@ -8,6 +8,6 @@ lambda(function ($event) {
     $kernel = new \App\Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
     $kernel->boot();
 
-    $sqsConsumer = $kernel->getContainer()->get(SqsConsumer::class);
-    $sqsConsumer->consumeLambdaEvent($event);
+    $worker = $kernel->getContainer()->get(BrefWorker::class);
+    $worker->consumeLambdaEvent($event);
 });
