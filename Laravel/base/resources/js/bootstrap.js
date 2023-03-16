0c72eaeb7e1311232032e1ec7b1a3dd7039c5c14
Matthieu Napoli <matthieu@mnapoli.fr>
2023-03-16 16:31:46 +0100

    Update Laravel example to v10 and Bref v2

--- Laravel/base/resources/js/bootstrap.js
+++ Laravel/base/resources/js/bootstrap.js
@@ -1,6 +1,3 @@
-import _ from 'lodash';
-window._ = _;
-
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
@@ -26,6 +23,7 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 // window.Echo = new Echo({
 //     broadcaster: 'pusher',
 //     key: import.meta.env.VITE_PUSHER_APP_KEY,
+//     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
 //     wsHost: import.meta.env.VITE_PUSHER_HOST ? import.meta.env.VITE_PUSHER_HOST : `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
 //     wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
 //     wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
